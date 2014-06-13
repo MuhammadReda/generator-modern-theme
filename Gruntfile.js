b62997e7087e0a07a176b8637d2f330ec8fbc0e4
Muhammad Reda <say.hello@mreda.net>
2014-06-14 01:53:38 +0200

    Setup grunt tasks: clean, compass, concat, ugligy & cssmin.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,77 @@
+'use strict';
+var _ = require('underscore');
+
 module.exports = function(grunt) {
+
+    var tasks = {};
+    tasks['concat'] = {
+        options: {
+            separator: '\n'
+        }
+    };
+
+    var themeConfig = grunt.file.readJSON('themeConfig.json');
+    _.each(themeConfig.sources.js, function(item, index, list) {
+        tasks.concat['js-' + index] = {};
+        tasks.concat['js-' + index]['src'] = item;
+        tasks.concat['js-' + index]['dest'] = '.tmp/js/' + index + '.min.js';
+    });
+    _.each(themeConfig.sources.css, function(item, index, list) {
+        tasks.concat['css-' + index] = {};
+        tasks.concat['css-' + index]['src'] = item;
+        tasks.concat['css-' + index]['dest'] = '.tmp/css/' + index + '.min.css';
+    });
+
+
 
     require('load-grunt-tasks')(grunt, {
         scope: ['devDependencies']
     });
 
     grunt.initConfig({
+        clean: {
+            temp: [
+                '.tmp/*'
+            ],
+            build: [
+                'assets/js/*',
+                'assets/css/*'
+            ]
+        },
 
+        compass: {
+            target: {
+                options: {
+                    config: 'config.rb'
+                }
+            }
+        },
+
+        concat: tasks.concat,
+
+        uglify: {
+            options: {
+                mangle: false
+            },
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: '.tmp/js/',
+                    src: '**/*.js',
+                    dest: themeConfig.destinations.js
+                }]
+            }
+        },
+
+        cssmin: {
+            target: {
+                expand: true,
+                cwd: '.tmp/css/',
+                src: '**/*.css',
+                dest: themeConfig.destinations.css
+            }
+        }
     });
 
-    grunt.registerTask('default', []);
+    grunt.registerTask('default', ['clean', 'concat', 'uglify', 'cssmin']);
 };
