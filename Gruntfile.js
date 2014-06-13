d4af6f36409e3b205e05764921bb1d3c2b8c66b0
Muhammad Reda <say.hello@mreda.net>
2014-06-13 19:43:07 +0200

    Added Gruntfile.

---
+++ Gruntfile.js
@@ -0,0 +1,12 @@
+module.exports = function(grunt) {
+
+    require('load-grunt-tasks')(grunt, {
+        scope: ['devDependencies']
+    });
+
+    grunt.initConfig({
+
+    });
+
+    grunt.registerTask('default', []);
+};
